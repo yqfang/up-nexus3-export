9fa197250f335a14bfbd3af7a845ab0c1e172fe3
Aron <fangyuqing@MacBook-Pro.local>
2023-06-21 15:53:23 +0800

    fix

--- nexus-import.sh
+++ nexus-import.sh
@@ -7,7 +7,11 @@ while getopts ":r:u:p:" opt; do
 	case $opt in
 		r) REPO_URL="$OPTARG"
 		;;
+		u) USERNAME="$OPTARG"
+		;;
+		p) PASSWORD="$OPTARG"
+		;;
 	esac
 done
 
-find . -type f -not -path './mavenimport\.sh*' -not -path '*/\.*' -not -path '*/\^archetype\-catalog\.xml*' -not -path '*/\^maven\-metadata\-local*\.xml' -not -path '*/\^maven\-metadata\-deployment*\.xml' | sed "s|^\./||" | xargs -I '{}' curl -X PUT -v -T {} ${REPO_URL}/{} ;
+find . -type f -not -path './mavenimport\.sh*' -not -path '*/\.*' -not -path '*/\^archetype\-catalog\.xml*' -not -path '*/\^maven\-metadata\-local*\.xml' -not -path '*/\^maven\-metadata\-deployment*\.xml' | sed "s|^\./||" | xargs -I '{}' curl -u "$USERNAME:$PASSWORD" -X PUT -v -T {} ${REPO_URL}/{} ;
